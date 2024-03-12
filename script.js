36ff988fbbbddee668d3e22af8a6126bf25be3db
rodolfo <rodolfovasconcellos88@gmail.com>
2024-03-12 16:36:35 -0300

    atualiando projeto

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
-let inputAdultos = document.getElementById("adultos");
-let inputCriancas = document.getElementById("criancas");
-let inputDuracao = document.getElementById("duracao");
+// Carne - 400gr por pessoa + de 6horas - 650
+// Cerveja - 1200ml por Pessoa + 6 horas - 2000ml
+// Refrigerante/água - 1000ml por pessoa + 6 horas 1500ml
 
-let resultado = document.getElementById("resultado");
+// Crianças valem por 0,5
 
-function calcular() {
-  console.log("Calculando...");
+const inputAdultos = document.getElementById("adultos");
+const inputCriancas = document.getElementById("criancas");
+const inputDuracao = document.getElementById("duracao");
 
-  let adultos = inputAdultos.value;
-  let criancas = inputCriancas.value;
-  let duracao = inputDuracao.value;
-
-  let qtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas;
-  let qtTotalCerveja = cervejaPP(duracao) * adultos;
-  let qtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2) * criancas;
-
-  resultado.innerHTML = `<p>${qtTotalCarne / 1000} Kg de Carne</p>`;
-  resultado.innerHTML += `<p>${Math.ceil(qtTotalCerveja / 355)} Latas de Cerveja</p>`;
-  resultado.innerHTML += `<p>${Math.ceil(qtTotalBebidas / 2000)}garrafas de Bebida</p>`;
-}
+const resultado = document.getElementById("resultado")
 
 function carnePP(duracao) {
-  if (duracao >= 6) {
+  
+  if(duracao >= 6) {
     return 650;
-  } else {
+  }else{
     return 400;
   }
 }
 
 function cervejaPP(duracao) {
-  if (duracao >= 6) {
+  
+  if(duracao >= 6) {
     return 2000;
-  } else {
+  }else{
     return 1200;
   }
 }
 
 function bebidasPP(duracao) {
-  if (duracao >= 6) {
-    return 2000;
-  } else {
-    return 1200;
+  
+  if(duracao >= 6) {
+    return 1500;
+  }else{
+    return 1000;
   }
+}
+
+function calcular() {
+  const adultos = inputAdultos.value;
+  const criancas = inputCriancas.value;
+  const duracao = inputDuracao.value;
+
+  const qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+  const qdtTotalCerveja = cervejaPP(duracao) * adultos;
+  const qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+  //coloca-se o "+" após o sinal de "=" para deixar entendido que preciso imprimir mais de um innerHTML
+  resultado.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`
+  resultado.innerHTML += `
+    <div class="result-block">
+      <img src="./assets/img/carne.svg"/>
+      <p>${qdtTotalCarne/1000} Kg de Carne</p>
+    </div>
+  `
+  resultado.innerHTML += `
+    <div class="result-block">
+      <img src="./assets/img/cerveja.svg"/>
+      <p>${Math.ceil(qdtTotalCerveja/355)} latas de Cerveja</p>
+    </div>
+  `
+  resultado.innerHTML += `
+    <div class="result-block">
+      <img src="./assets/img/refri.svg"/>
+      <p>${Math.ceil(qdtTotalBebidas/2000)} garrafas de Bebidas</p>
+    </div>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+  `
 }
